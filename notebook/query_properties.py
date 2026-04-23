d7fe03a0f70c6cfbb140d495c81579460f46c3b9
zilinwang <ziw143@pitt.edu>
2026-04-23 14:58:37 -0400

    result 1-1000

--- notebook/query_properties.py
+++ notebook/query_properties.py
@@ -8,8 +8,8 @@ from openai import OpenAI
 # Config
 # ─────────────────────────────────────────────
 
-IMAGE_DIR    = "../data/gsv_out/first_200"   # update to your image folder on the server
-RESULTS_FILE = "../result/result_2.json" 
+IMAGE_DIR    = "../data/gsv_out/gsv-data"   # update to your image folder on the server
+RESULTS_FILE = "../result/result_1-1000.json" 
 MODEL        = "/u/capstone/hf_cache/Qwen3.5-35B-A3B"
 
 client = OpenAI(
